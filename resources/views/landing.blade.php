254ec012aa46b57db70f9ddf47e404f52768e2d8
aldergutierrez <aldergutierrez@outlook.com>
2018-05-01 15:44:46 -0500

    Initial commit

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>DevRev</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/boostrap/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/style_prueba.css" rel="stylesheet">
+  </head>
+
+  <body>
+<div>                                                    
+ 
+</div> 
+<nav class="navbar navbar-inverse">
+      <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+ <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+         <a class="navbar-brand  " href="#">
+        <img  alt="DevRev" src="Brand/revdev_logo2.png" width="50px" >
+      </a>
+    </div>
+    <div class="collapse navbar-collapse" id="navbar-1"> 
+        <ul class="nav navbar-nav">
+        <li><a href="#">Link</a></li>
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+             </li>
+      </ul>
+ <form class="navbar-form navbar-right " role="search">
+        <div class="form-group">
+        <input type="text" class="form-control " placeholder="Search">
+        <button class="btn btn-primary" type="submit"><span class="glyphicon glyphicon-search"></span></button>
+        </div>
+        </form>
+        <ul class="nav navbar-nav navbar-right ">
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li> 
+           
+      </ul> 
+
+
+     </div><!-- /.navbar-collapse -->
+
+
+
+
+      </div><!-- /.container-fluid -->
+    </nav>
+
+    <main role="main">
+      <!-- Main jumbotron for a primary marketing message or call to action -->
+      <div class="jumbotron">
+        <div class="container-fluid">
+          
+          <h1 class="display-3">Showcase n Dev Videogames</h1>
+          <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+          <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+        </div>
+     
+      </div>
+
+      <div class="container">
+        <!-- Example row of columns -->
+        <div class="row">
+          <div class="col-md-3 post">
+            <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+            <p class="noteTitle row">Donec id elit</p>
+            <p class="noteSub row">gravida at eget metus. Fusce dapibus, tellus ac cursus commodo</p>
+            <div class="interaction input-group center-block row ">
+           <a class="btn btn-secondary col-md-4" href="#" style="background-color: #424242">AAA</a>
+           <a class="btn btn-secondary col-md-4" href="#" style="background-color: #424242">AAA</a>
+           <a class="btn btn-secondary col-md-4" href="#" style="background-color: #424242">AAA</a>
+         </div>
+          </div>
+          <div class="col-md-3 post">
+            <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+            <p class="noteTitle row">Donec id elit</p>
+            <p class="noteSub row">gravida at eget metus. Fusce dapibus, tellus ac cursus commodo</p>
+            <div class="interaction input-group center-block row ">
+           <a class="btn btn-secondary col-md-4" href="#" style="background-color: #424242">AAA</a>
+           <a class="btn btn-secondary col-md-4" href="#" style="background-color: #424242">AAA</a>
+           <a class="btn btn-secondary col-md-4" href="#" style="background-color: #424242">AAA</a>
+         </div>
+          </div>
+
+          <div class="col-md-3 post">
+            <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+            <p class="noteTitle row">Donec id elit</p>
+            <p class="noteSub row">gravida at eget metus. Fusce dapibus, tellus ac cursus commodo</p>
+            <div class="interaction input-group center-block row ">
+           <a class="btn btn-secondary col-md-4" href="#" style="background-color: #424242">AAA</a>
+           <a class="btn btn-secondary col-md-4" href="#" style="background-color: #424242">AAA</a>
+           <a class="btn btn-secondary col-md-4" href="#" style="background-color: #424242">AAA</a>
+         </div>
+          </div>
+
+          <div class="col-md-3 post">
+            <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+            <p class="noteTitle row">Donec id elit</p>
+            <p class="noteSub row">gravida at eget metus. Fusce dapibus, tellus ac cursus commodo</p>
+            <div class="interaction input-group center-block row ">
+           <a class="btn btn-secondary col-md-4" href="#" style="background-color: #424242">AAA</a>
+           <a class="btn btn-secondary col-md-4" href="#" style="background-color: #424242">AAA</a>
+           <a class="btn btn-secondary col-md-4" href="#" style="background-color: #424242">AAA</a>
+         </div>
+          </div>
+
+          <div class="col-md-3 post">
+            <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+            <p class="noteTitle row">Donec id elit</p>
+            <p class="noteSub row">gravida at eget metus. Fusce dapibus, tellus ac cursus commodo</p>
+            <div class="interaction input-group center-block row ">
+           <a class="btn btn-secondary col-md-4" href="#" style="background-color: #424242">AAA</a>
+           <a class="btn btn-secondary col-md-4" href="#" style="background-color: #424242">AAA</a>
+           <a class="btn btn-secondary col-md-4" href="#" style="background-color: #424242">AAA</a>
+         </div>
+          </div>
+
+          <div class="col-md-3 post">
+            <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+            <p class="noteTitle row">Donec id elit</p>
+            <p class="noteSub row">gravida at eget metus. Fusce dapibus, tellus ac cursus commodo</p>
+            <div class="interaction input-group center-block row ">
+           <a class="btn btn-secondary col-md-4" href="#" style="background-color: #424242">AAA</a>
+           <a class="btn btn-secondary col-md-4" href="#" style="background-color: #424242">AAA</a>
+           <a class="btn btn-secondary col-md-4" href="#" style="background-color: #424242">AAA</a>
+         </div>
+          </div>
+
+          <div class="col-md-3 post">
+            <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+            <p class="noteTitle row">Donec id elit</p>
+            <p class="noteSub row">gravida at eget metus. Fusce dapibus, tellus ac cursus commodo</p>
+            <div class="interaction input-group center-block row ">
+           <a class="btn btn-secondary col-md-4" href="#" style="background-color: #424242">AAA</a>
+           <a class="btn btn-secondary col-md-4" href="#" style="background-color: #424242">AAA</a>
+           <a class="btn btn-secondary col-md-4" href="#" style="background-color: #424242">AAA</a>
+         </div>
+          </div>
+
+          <div class="col-md-3 post">
+            <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+            <p class="noteTitle row">Donec id elit</p>
+            <p class="noteSub row">gravida at eget metus. Fusce dapibus, tellus ac cursus commodo</p>
+            <div class="interaction input-group center-block row ">
+           <a class="btn btn-secondary col-md-4" href="#" style="background-color: #424242">AAA</a>
+           <a class="btn btn-secondary col-md-4" href="#" style="background-color: #424242">AAA</a>
+           <a class="btn btn-secondary col-md-4" href="#" style="background-color: #424242">AAA</a>
+         </div>
+          </div>
+        </div>
+
+        <hr>
+
+      </div> <!-- /container -->
+
+    </main>
+
+    <footer class="container">
+   
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+ <!--    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>-->
+    </div>
+     <script src="jquery/jquery-3.3.1.min.js"></script> 
+    <script src="js/holder.min.js"></script> 
+    <script src="css/boostrap/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
