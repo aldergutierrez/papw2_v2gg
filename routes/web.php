43e2de42d69c4e0b271ff2f2d87486bb02488ea0
caolmad95@gmail.com <killjoy95>
2018-05-25 01:11:14 -0500

    no se que verga
    va a valer verga este pedo

--- routes/web.php
+++ routes/web.php
@@ -65,3 +65,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
