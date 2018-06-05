a5f8b941e574a02e1f4e2e181a4f6f7a55551e6e
Alder Gutierrez <aldergutierrez@outlook.com>
2018-06-05 17:41:26 -0500

    Revert "post terminado"
    This reverts commit a8b65e061f759f32b2d2e37c1fe0b60b762337db.

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -14,7 +14,7 @@
                   <li class="post-account">aldergutierrez</li>
                   <li class="post-category">creativeField </li>
                 </ul>
-                <img src="../img/Pic1.jpg">
+                <img src="img/">
                 <div class="btn-follow btn">Follow</div>
                 <ul class="likes">
                   <li><span class="glyphicon glyphicon-heart"></span></li>
@@ -26,13 +26,12 @@
                 </ul>
               <div class="date">Published: created_at </div>
               <div class="border"></div>
-              <span class="TxtHola4">Fellas n Tools</span>
               <img class="tool" src="../img/Pic1.jpg">
               <div class="border"></div>
-
               <div class="btn btn-like">Like project</div>
-
               <div class="date">Comments(20)</div>
+              <textarea class="txt-comment"></textarea>
+              <div class="btn btn-like">Post comment</div>
               <div class="comments">
               <img class="img-comment" src="../img/Pic1.jpg">
               <div class="txt-date">12:34am</div>
@@ -56,12 +55,6 @@
  
               </div>
               </div>
-
-              
-              <textarea class="txt-comment"></textarea>
-              <div class="btn btn-like">Post comment</div>
-
-              
         </div>
           @endsection
 
