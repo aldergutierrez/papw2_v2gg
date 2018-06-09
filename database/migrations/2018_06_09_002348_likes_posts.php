d88b1d1c5371cd546dd0f8570df9d3907ef56034
caolmad95@gmail.com <killjoy95>
2018-06-09 17:06:33 -0500

    likes table
    migration

---
+++ database/migrations/2018_06_09_002348_likes_posts.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class LikesPosts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+   {
+        Schema::create('likes_posts', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->unsignedInteger('idUser');
+            $table->unsignedInteger('idPost');
+            $table->timestamps();
+         
+        });
+
+            Schema::table('likes_posts', function (Blueprint $table) {
+            $table->foreign('idUser')->references('id')->on('users');
+            $table->foreign('idPost')->references('id')->on('posts');
+            });
+    
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('likes_posts');
+    }
+}
